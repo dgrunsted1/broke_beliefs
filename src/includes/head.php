d5aecabeb53c31fdf8d1954be201e1b199f0d446
David <dgrunsted1@gmail.com>
2021-08-30 20:26:28 -0500

    boilerplate stuff

---
+++ src/includes/head.php
@@ -0,0 +1,9 @@
+<title><?php print $PAGE_TITLE;?></title>
+
+<?php if ($CURRENT_PAGE == "Index") { ?>
+	<meta name="description" content="" />
+	<meta name="keywords" content="" /> 
+<?php } ?>
+
+
+<link rel="stylesheet" href="css/main.css">
---
+++ src/includes/header.php
@@ -0,0 +1,4 @@
+
+<div class="jumbotron">
+	<h1>Hello there!</h1>
+</div>
