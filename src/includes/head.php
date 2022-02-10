19b06484c0a4d481cfe0ecd7ed80b55f555d9b27
dgrunsted1 <dgrunsted1@gmail.com>
2022-02-09 23:51:28 -0600

    wordle

--- src/includes/head.php
+++ src/includes/head.php
@@ -5,5 +5,5 @@
 	<meta name="keywords" content="" /> 
 <?php } ?>
 
-
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
 <link rel="stylesheet" href="css/main.css">
